ecef361e7d9571a5e59a77589a4f95fc91c2e7a3
unknown <andres@digitalhouse.com>
2018-10-19 21:53:16 -0300

    Recycler View

---
+++ app/src/main/java/com/example/aula_01/revisaorecyclerview/model/Pessoa.java
@@ -0,0 +1,28 @@
+package com.example.aula_01.revisaorecyclerview.model;
+
+public class Pessoa {
+    private String nome;
+    private String idade;
+
+    public Pessoa(String nome, String idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+}
+
